bff566db6a986e2752e68d6543508d00e92e774e
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-10-09 20:34:07 -0400

    Add eslint configs from assignment 2

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,40 +1,28 @@
-import js from '@eslint/js';
 import globals from 'globals';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import js from '@eslint/js';
 
 export default [
-  { ignores: ['dist'] },
+  // files to ignore entirely
   {
-    files: ['**/*.{js,jsx}'],
+    ignores: [
+      '**/node_modules/*',
+      '**/dist/*',
+      '**/build/*',
+      '**/webpack.config.{js,cjs}',
+      'eslint.config.mjs'
+    ]
+  },
+  //use recommended rules as a base for all files and then override
+  js.configs.recommended,
+  //override rules for all files
+  {
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module'
-      }
-    },
-    settings: { react: { version: '19.1' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
+      globals: {
+        ...globals.node
+      },
+      sourceType: 'module'
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ],
-      // ADDITIONAL RULES
-      'react/prop-types': 'off',
       ////////// Possible Errors //////////
       'no-console': ['warn', { allow: ['error'] }],
       'one-var': ['warn', 'never'],
