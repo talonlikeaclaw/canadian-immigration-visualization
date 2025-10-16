ab715788b28afe2e52f801fa94b6d618fab4da1b
Habib <habibullah.safari7@gmail.com>
2025-10-16 11:04:29 -0400

    Add city profile route

--- server/routers/city.mjs
+++ server/routers/city.mjs
@@ -3,9 +3,19 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/:city_name', (req, res) => {
-  const { cityName } = req.params;
-  if (cityName) {
-    res.json({ message: ` you asked for ${cityName} city` });
+  const { city_name } = req.params;
+  if (city_name) {
+    res.json({ message: ` you asked for ${city_name} city` });
+  } else {
+    res.status(404).json({ error: 'Router: city not found ' });
+  }
+});
+
+
+router.get('/:city/profile', (req, res) => {
+  const { city_name } = req.params;
+  if (city_name) {
+    res.json({ message: ` combined profile for ${city_name} city` });
   } else {
     res.status(404).json({ error: 'Router: city not found ' });
   }
--- server/routers/languages.mjs
+++ server/routers/languages.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/:city', (req, res) => {
   
   // } else {
   //   res.status(404).json({ error: 'Router: languages data not found' });
