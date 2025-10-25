5948d0b17b359f0d7298af90813e7e1f776e374a
Melania Chiru <melania@chiru.ca>
2025-10-25 11:51:36 -0400

    define skeleton for extra routes

--- server/routers/immigrationRouter.mjs
+++ server/routers/immigrationRouter.mjs
@@ -20,4 +20,32 @@ router.get('/:city/period/:period', (req, res) => {
   }
 });
 
+router.get('/:city/country/:country', (req, res) => {
+  const { city, country } = req.params;
+  if (city) {
+    res.json({ message: `Specific immigration pattern of ${country} into ${city} city` });
+  } else {
+    res.status(404).json({ error: 'Router: city not found ' });
+  }
+});
+
+// Limit to top 5-10 countries that have immigrated the most to given city
+router.get('/:city/topCountries', (req, res) => {
+  const { city } = req.params;
+  if (city) {
+    res.json({ message: `Most countries emmigrating towards ${city} city` });
+  } else {
+    res.status(404).json({ error: 'Router: city not found ' });
+  }
+});
+
+router.get('/:city/summary', (req, res) => {
+  const { city } = req.params;
+  if (city) {
+    res.json({ message: `Summary of stats for ${city} city` });
+  } else {
+    res.status(404).json({ error: 'Router: city not found ' });
+  }
+});
+
 export default router;
