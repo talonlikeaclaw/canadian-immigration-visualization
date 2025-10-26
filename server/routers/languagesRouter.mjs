b04b7961a8c5653ee8a32f22d0f3a17b8de23e23
habib <habibullah.safari7@gmail.com>
2025-10-25 20:12:42 -0400

    Change error code 404 to 400

--- server/routers/languagesRouter.mjs
+++ server/routers/languagesRouter.mjs
@@ -8,7 +8,7 @@ router.get('/:cityName', async (req, res) => {
   try {
     let { cityName } = req.params;
     if (!cityName) {
-      return res.status(404).json({ error: 'language router : city name is required!' });
+      return res.status(400).json({ error: 'language router : city name is required!' });
     }else if (cityName.toLowerCase() === 'montreal') {
       // a quixk fix for now => in db montreal has accent
       cityName = 'Montréal';
