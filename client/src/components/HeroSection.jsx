54f1826888d8856f339798642c34698dd19bbe57
Melania Chiru <melania@chiru.ca>
2025-10-25 22:39:51 -0400

    define HeroSection component and display info

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,19 @@
+function HeroSection(){
+  return(
+    <section className="hero-section">
+      <h1>A Linguistic Journey from Coast to Coast</h1>
+      <p> 
+        Immigration has always shaped Canada, 
+        but the nature of that impact is distinct in every region.
+      </p>
+      <p>
+        Follow us on a geographic journey across Canada, 
+        starting in the Maritime provinces and moving westward to the Pacific
+         as we learn about vibrant, diverse language communities that define our largest cities, 
+         revealing how global migration has reshaped Canada&apos;s identity in the 21st century.
+      </p>
+    </section>
+  );
+}
+
+export default HeroSection;
