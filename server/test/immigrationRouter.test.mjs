1eba35781e7e5d03dedb21b92b4caad6b7309201
Melania Chiru <melania@chiru.ca>
2025-10-27 20:16:00 -0400

    add unit tests for /:city/period/:start/:end

--- server/test/immigrationRouter.test.mjs
+++ server/test/immigrationRouter.test.mjs
@@ -195,3 +195,110 @@ describe('GET /:city/period/:end', ()=>{
   });
 
 });
+
+describe('GET /:city/period/:start/:end', ()=>{
+  let setCollectionStub;
+  let findStub;
+
+  beforeEach(()=>{
+    // stub db call with fake call
+    setCollectionStub = sinon.stub(db, 'setCollection').resolves();
+
+    // stub db.find
+    // by default will return empty array (no data)
+    findStub = sinon.stub(db, 'find').resolves([]);
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  // Act
+  it('Should return num of immigrants by country for city, when valid period passed', async()=>{
+    const mockImmigrationData = [
+      {
+        '_id': {
+          '$oid': '68fce6a8a27b1de7e01e6505'
+        },
+        'City': 'Halifax (CMA), N.S.',
+        'Country': 'United States of America',
+        'Period': 'Before 1980',
+        'Count': 1340
+      },
+      {
+        '_id': {
+          '$oid': '68fce6a8a27b1de7e01e6506'
+        },
+        'City': 'Halifax (CMA), N.S.',
+        'Country': 'United States of America',
+        'Period': '1980 to 1990',
+        'Count': 460
+      },
+      {
+        '_id': {
+          '$oid': '68fce6a8a27b1de7e01e6507'
+        },
+        'City': 'Halifax (CMA), N.S.',
+        'Country': 'United States of America',
+        'Period': '1991 to 2000',
+        'Count': 305
+      }
+    ];
+    // resolve to correct, expected data
+    findStub.resolves(mockImmigrationData);
+    const response = await request(app).get('/api/immigration/halifax/period/2001/2005');
+
+    // response structure 
+    expect(response.statusCode).to.equal(200);
+    expect(response.body).to.be.an('object');
+
+    // response object properties
+    expect(response.body).to.have.property('city');
+    expect(response.body).to.have.property('period');
+    expect(response.body).to.have.property('totalImmigrants');
+    expect(response.body).to.have.property('countries');
+    expect(Object.keys(response.body.countries).length).to.be.greaterThan(0);
+
+    // stub behavious
+    expect(findStub.calledOnce).to.be.true;
+    expect(setCollectionStub.calledWith('immigration')).to.be.true;
+  });
+
+  it('Should return 400 - Bad request for invalid city name', async()=>{
+    const response = await request(app).get('/api/immigration/halifax123/period/2001/2005');
+
+    // Assert
+    expect(response.statusCode).to.equal(400);
+    expect(response.body).to.have.property('error').equal('Invalid city name');
+    expect(findStub.called).to.be.false;
+  });
+
+  it('Should return 400 - Bad Request for an invalid starting year', async () => {
+    const response = await request(app).get('/api/immigration/halifax/period/2002a/2005');
+    // Assert
+    expect(response.statusCode).to.equal(400);
+    expect(response.body).to.have.property('error').equal('Invalid starting year');
+    expect(findStub.called).to.be.false;
+  });
+
+  it('Should return 400 - Bad Request for an invalid ending year', async () => {
+    const response = await request(app).get('/api/immigration/halifax/period/2001/abc');
+    // Assert
+    expect(response.statusCode).to.equal(400);
+    expect(response.body).to.have.property('error').equal('Invalid ending year');
+    expect(findStub.called).to.be.false;
+  });
+
+  it('Should return 404 - Not Found if no data is returned from DB', async () => {
+    // findStub is already configured to return [] by default in beforeEach()
+    const response = await request(app).get('/api/immigration/halifax/period/2002/2005');
+
+    // Assert
+    expect(response.statusCode).to.equal(404);
+    expect(response.body).to.have.
+      property('error').
+      include('No immigration data found for halifax in period 2002 to 2005.');
+    expect(findStub.calledOnce).to.be.true;
+  });
+
+});
