a61ca7f55694608aa869c6505622125825432024
Habibullah Safari <habibullah.safari7@gmail.com>
2025-11-07 16:47:53 -0500

    Use eslint

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import '../assets/styles/Footer.css';
 export default function Footer(){
-    return(
-        <footer className="footer">
+  return(
+    <footer className="footer">
              © Fall 2025, team: Melania Chiru, Talon Dunbar, Habibullah Safari
-        </footer>
-    )
+    </footer>
+  );
 }
