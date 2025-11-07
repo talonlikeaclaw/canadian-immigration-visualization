e922c3494b61f4077cc19a2261a5f922860a1a84
Habibullah Safari <habibullah.safari7@gmail.com>
2025-11-07 16:31:58 -0500

    Add footer style

--- client/src/components/Footert.jsx
+++ client/src/components/Footert.jsx
@@ -1,6 +1,7 @@
+import '../assets/styles/Footer.css';
 export default function Footer(){
     return(
-        <footer>
+        <footer className="footer">
              © Fall 2025, team: Melania Chiru, Talon Dunbar, Habibullah Safari
         </footer>
     )
