5ef15981a56446e6b1d0beb352cdfba428b215b4
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-11-08 12:01:40 -0500

    Add city_data.mjs with 2021 census city information

---
+++ server/db/utils/data/city_data.mjs
@@ -0,0 +1,38 @@
+export const cityData = {
+  halifax: {
+    Province: 'Nova Scotia',
+    Population: 439819,
+    AreaKm2: 5475.57,
+    Geolocation: [44.6475, -63.59056]
+  },
+  toronto: {
+    Province: 'Ontario',
+    Population: 2794356,
+    AreaKm2: 631.1,
+    Geolocation: [43.65322, -79.38318]
+  },
+  montreal: {
+    Province: 'Quebec',
+    Population: 1762949,
+    AreaKm2: 364.74,
+    Geolocation: [45.50169, -73.56725]
+  },
+  calgary: {
+    Province: 'Alberta',
+    Population: 1306784,
+    AreaKm2: 820.62,
+    Geolocation: [51.04473, -114.07188]
+  },
+  edmonton: {
+    Province: 'Alberta',
+    Population: 1010899,
+    AreaKm2: 765.61,
+    Geolocation: [53.54613, -113.49382]
+  },
+  vancouver: {
+    Province: 'British Columbia',
+    Population: 662248,
+    AreaKm2: 115.18,
+    Geolocation: [49.28273, -123.12074]
+  }
+};
