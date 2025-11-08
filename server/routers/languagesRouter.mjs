ec92e21b4983d8e24c92c59208ea3d37286b6d89
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-11-08 15:36:42 -0500

    Add 500 server error response to language swagger doc

--- server/routers/languagesRouter.mjs
+++ server/routers/languagesRouter.mjs
@@ -61,6 +61,14 @@ const router = express.Router();
  *                   example: "no language data found for Montréal"
  *       500:
  *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Internal server error.
  */
 router.get('/:city', async (req, res, next) => {
   try {
