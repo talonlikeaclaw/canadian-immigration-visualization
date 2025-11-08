8eab8c306808a666cfee0a97b1534c9a58541ed9
Habibullah Safari <habibullah.safari7@gmail.com>
2025-11-07 19:45:05 -0500

    Add anchors

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,35 @@ import '../assets/styles/Footer.css';
 export default function Footer(){
   return(
     <footer className="footer">
-             © Fall 2025, team: Melania Chiru, Talon Dunbar, Habibullah Safari
+        © Fall 2025, team: Melania Chiru, Talon Dunbar, Habibullah Safari
+        <br/>
+
+        Data source: {''}
+        <a
+            href=""
+            target="blank"
+            rel=""
+        >
+            Statistics Canada: Custom 2021 Census Immigration Data
+        </a> {''}
+        and {''}
+        <a 
+            href=""
+            target="blank"
+            rel=""
+        >
+            Statistics Canada: Custom 2021 Census Language Data
+        </a>
+        .<br/>
+        License under the {''}
+        <a
+            href=""
+            target="blank"
+            rel=""
+            >
+            Open Government License - Canada
+        </a>
+        .
     </footer>
   );
 }
