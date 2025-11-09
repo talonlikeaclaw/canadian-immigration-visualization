d6f2d5af64313fc92875e12d0b5aff4297e92cba
Melania Chiru <melania@chiru.ca>
2025-11-09 17:09:45 -0500

    display montreal map and info

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -10,10 +10,15 @@ function App() {
   // lead me to the react intersction observer (plus I think this was
   // in one of class lectures, with lazy loading)
   // https://www.npmjs.com/package/react-intersection-observer
-  const { ref: halifaxRef, inView: halifaxInView } = useInView();
+  const options = { threshold: 0.5 };
+
+  const { ref: halifaxRef, inView: halifaxInView } = useInView(options);
+  const { ref: montrealRef, inView: montrealInView } = useInView(options);
 
   // Determine the currently zoomed city based on scroll position
-  const currentZoomedCity = halifaxInView ? 'halifax' : '';
+  const currentZoomedCity = 
+  halifaxInView ? 'halifax' 
+    : montrealInView ? 'montreal' : '';
 
   return (
     <>
@@ -65,6 +70,57 @@ function App() {
         </p>
 
         <Chart city="halifax"></Chart>
+
+      </section>
+
+      <div className="city-divider"></div>
+
+      <p>
+        Next, we head to Montreal, the vibrant cultural heart of French North America.
+        This city&apos;s identity is defined by its powerful Francophone ties. 
+        Montreal’s exposure to the world got a huge boost from Expo 67, 
+        the spectacularly successful World’s Fair. It put Montréal on the global stage.</p>
+
+      <section className="montreal-section" ref={montrealRef}>
+        <p>
+          To protect its unique French identity, Quebec took control of immigration. 
+          A key moment was Bill 101 (the Charter of the French Language) in 1977, 
+          which made French the official language and the required language for immigrant 
+          childrens schooling.
+        </p>
+        <p>
+          This control was cemented with the 1991 Canada-Quebec Accord. 
+          This agreement gave the province almost total power to select its own
+          economic immigrants and manage their French integration ( francization ). 
+          The clear goal: to attract Francophones from across the globe.
+        </p>
+
+        <p>
+          This strategy worked and created an incredibly diverse mix. 
+          Between 2016 and 2021, over 161,700 new immigrants arrived.
+        </p>
+
+        <p>
+          The top countries of origin show a clear global Francophone
+          attraction: France (15,295), Algeria (12,840), and Syria (11,595) led the way.
+        </p>
+
+        <p>
+          This blend makes Montréal the only major Canadian city where French is the 
+          majority language, spoken by over 2.7 million people, while English also maintains 
+          a major presence with 693,340 speakers. The city&apos;s truly cosmopolitan nature is 
+          further reflected in its leading non-official languages, where Spanish (90,235 speakers)
+          and Arabic (89,800 speakers) indicate strong links to Latin America, the Middle East, 
+          and North Africa.
+        </p>
+
+        <p>
+          However, European languages also have a deep historical footprint, 
+          with large communities of Italian (25,805 speakers), Russian (21,015 speakers), 
+          and Romanian (20,590 speakers) speakers.
+        </p>
+
+        <Chart city="montréal"></Chart>
 
       </section>
 
