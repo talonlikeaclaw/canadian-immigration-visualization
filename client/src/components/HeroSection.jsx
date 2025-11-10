094feea11bf08b0f62de7bd4d92b76ae31e26bfe
Melania Chiru <melania@chiru.ca>
2025-11-09 22:52:29 -0500

    style hero section

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,17 +1,27 @@
+import '../assets/styles/HeroSection.css';
+import arrow from'../assets/images/arrow.svg';
+
 function HeroSection(){
   return(
     <section className="hero-section">
       <h1>A Linguistic Journey from Coast to Coast</h1>
-      <p> 
-        Immigration has always shaped Canada, 
-        but the nature of that impact is distinct in every region.
-      </p>
-      <p>
-        Follow us on a geographic journey across Canada, 
-        starting in the Maritime provinces and moving westward to the Pacific
-         as we learn about vibrant, diverse language communities that define our largest cities, 
-         revealing how global migration has reshaped Canada&apos;s identity in the 21st century.
-      </p>
+      <section className="mini-intro">
+        <p> 
+          Immigration has always shaped Canada, 
+          but the nature of that impact is distinct in every region.
+        </p>
+        <p>
+          Follow us on a geographic journey across Canada, 
+          starting in the Maritime provinces and moving westward to the Pacific
+          as we learn about vibrant, diverse language communities that define our largest cities, 
+          revealing how global migration has reshaped Canada&apos;s identity in the 21st century.
+        </p>
+      </section>
+
+      <section className="scroll">
+        <p>Scroll to begin journey</p>
+        <img src={arrow} alt="downwards facing arrow" />
+      </section>
     </section>
   );
 }
