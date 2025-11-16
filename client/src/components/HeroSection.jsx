a5acced05fb6824aacf2ec8d22572c289e825bca
Melania Chiru <melania@chiru.ca>
2025-11-16 14:03:13 -0500

    clean up hero section styling

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -7,24 +7,27 @@ function HeroSection(){
   return(
     <section className="hero-section">
       <img src={blueWaves} alt="gradiant blue waves" className="blue-waves" />
-      <h1>A Linguistic Journey from Coast to Coast</h1>
-      <section className="mini-intro">
-        <p> 
-          Immigration has always shaped Canada, 
-          but the nature of that impact is distinct in every region.
-        </p>
-        <p>
-          Follow us on a geographic journey across Canada, 
-          starting in the Maritime provinces and moving westward to the Pacific
-          as we learn about vibrant, diverse language communities that define our largest cities, 
-          revealing how global migration has reshaped Canada&apos;s identity in the 21st century.
-        </p>
+      <section className="hero-section__content">
+        <h1>A Linguistic Journey from Coast to Coast</h1>
+        <section className="mini-intro">
+          <p> 
+            Immigration has always shaped Canada, 
+            but the nature of that impact is distinct in every region.
+          </p>
+          <p>
+            Follow us on a geographic journey across Canada, 
+            starting in the Maritime provinces and moving westward to the Pacific
+            as we learn about vibrant, diverse language communities that define our largest cities, 
+            revealing how global migration has reshaped Canada&apos;s identity in the 21st century.
+          </p>
+        </section>
+
+        <section className="scroll">
+          <p>Scroll to begin journey</p>
+          <img src={arrow} alt="downwards facing arrow" />
+        </section>
       </section>
 
-      <section className="scroll">
-        <p>Scroll to begin journey</p>
-        <img src={arrow} alt="downwards facing arrow" />
-      </section>
     </section>
   );
 }
