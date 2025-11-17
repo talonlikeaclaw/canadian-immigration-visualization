0195b51db2e4d78fa8dbe9493c536feb42b48c33
Melania Chiru <melania@chiru.ca>
2025-11-17 13:34:19 -0500

    switch from plotly.js to chart.js

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -1,64 +1,77 @@
 import { memo } from 'react';
-import Plot from 'react-plotly.js';
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+// Register the necessary Chart.js components
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function Chart({ data, title, xLabel, yLabel }) {
   if (!data || data.length === 0) {
     return <span>There was an error fetching the data. <br/> No data available to display.</span>;
   }
 
-  const labels = data.map(item => item.label);
-  const values = data.map(item => item.value);
+  const labels = data.map(item => item.label).reverse();
+  const values = data.map(item => item.value).reverse();
 
-  const plotData = [
-    {
-      y: labels,
-      x: values,
-      type: 'bar',
-      orientation: 'h',
-      marker: {
-        color: 'rgba(55,128,191,0.6)',
-        line: {
-          color: 'rgba(55,128,191,1.0)',
-          width: 1
-        }
-      }
-    }
-  ];
-
-  const layout = {
-    title,
-    xaxis: {
-      title: { title: xLabel }
-    },
-    yaxis: {
-      title: { title: yLabel, automargin: true }
-    },
-    margin: { l: 160, r: 20, t: 20, b: 20 },
-    autosize: true
+  const userData = {
+    labels: labels,
+    datasets: [{
+      data: values,
+      backgroundColor: '#F0F5F6',
+      borderColor: '#2D407A',
+      borderWidth: 1,
+    }]
   };
 
   return (
-    <Plot
-      data={plotData}
-      layout={layout}
-      config={{
-        responsive: true,
-        displaylogo: false,
-        modeBarButtonsToRemove: [
-          'zoomIn2d',
-          'zoomOut2d',
-          'autoScale2d',
-          'select2d',
-          'lasso2d',
-          'toggleSpikelines',
-          'hoverClosestCartesian',
-          'hoverCompareCartesian',
-          'pan2d'
-        ]
-      }}
-      style={{ width: '100%', height: '100%' }}
-    />
+    <div className="chart-wrapper">
+      <h2 style={{ textAlign: 'center' }}>{title}</h2>
+      <Bar
+        data={userData}
+        options={{
+          indexAxis: 'y',
+          plugins: {
+            title: {
+              display: false
+            },
+            legend: {
+              display: false
+            }
+          },
+          scales: {
+            x: {
+              title: {
+                display: true,
+                text: xLabel
+              },
+            },
+            y: {
+              title: {
+                display: true,
+                text: yLabel,
+              },
+              beginAtZero: true,
+            }
+          }
+        }}
+      />
+    </div>
   );
-}
+};
 
 export default memo(Chart);
